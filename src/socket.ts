a9b8169c106847b1bf5631b5be3b3542f2e1991b
AyushNag3 <69851334+AyushNag3@users.noreply.github.com>
2025-07-03 19:36:13 +0530

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -13,7 +13,6 @@ export const setupsocket = (server) => {
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"], // frontend site
-      credentials: true,
     },
   })
 
