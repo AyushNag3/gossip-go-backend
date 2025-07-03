22cd95ed7cfcaf63143e8280f9f468093ae9599d
AyushNag3 <69851334+AyushNag3@users.noreply.github.com>
2025-07-03 16:39:06 +0530

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -9,7 +9,7 @@ const prisma = new PrismaClient()
 export const setupsocket = (server) => {
   const io = new SocketIoServer(server, {
     cors: {
-      origin: "http://localhost:5173", // frontend site
+      origin: "*", // frontend site
       credentials: true,
     },
   })
