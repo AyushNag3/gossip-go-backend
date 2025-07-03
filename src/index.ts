6e7db12754ac220a72a22d6fb3e5eebd01d83aa6
AyushNag3 <69851334+AyushNag3@users.noreply.github.com>
2025-07-03 19:46:22 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-     origin: ["http://localhost:5173", "https://gossip-go.vercel.app/"],
+     origin: ["http://localhost:5173", "https://gossip-go.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
